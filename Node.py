95d4d6642ae2ecd85007568c47e84ede450b6771
CursedDani <dfarangog@gmail.com>
2023-05-12 05:34:12 -0500

    V1.0 of Node.py, still not fully functional

--- Node.py
+++ Node.py
@@ -7,7 +7,7 @@ def File(ndata:str)->tuple:
     return (ndata,name,ext,size)
     
 def Directory(nname:str,open=4) -> tuple:
-    name=nname
+    name=nname + "*"
     vopen=open
     return (name,vopen)
         
@@ -66,36 +66,76 @@ def searchDir(xs:tuple,sname:str)->bool:
                return True
           else:
                return False
-          
+'''        
 def findDir(xs:tuple,sname:str)->tuple:
     if data(data(xs)) == sname and len(data(xs)) == 2:
         return xs
     else:
         if n1(xs):
-            return findDir(n1(xs),sname)
+            return xs + findDir(n1(xs),sname)
         if n2(xs):
-            return findDir(n2(xs),sname)
+            return xs + findDir(n2(xs),sname)
         if n3(xs):
-             return findDir(n3(xs),sname)
+             return xs + findDir(n3(xs),sname)
         if n4(xs):
-             return findDir(n4(xs),sname)
+             return xs + findDir(n4(xs),sname)
+'''        
+
+
+
             
                
           
      
 
-
+'''
 def addB(ndata,xs:tuple,type:int,pardir:str)->tuple:
-    def addFile(ndata,xs,pardir):
-         if searchDir(xs,pardir):
-              xss=findDir(xs,pardir)
-              f = File(ndata)
-              xsy = addC(f,xss)
-              return xsy
+    def isDir(xs:tuple)->bool:
+        if len(data(xs)) == 2:
+             return True
+        else:
+             return False
+    
+    def isFile(xs:tuple)->bool:
+         if len(xs) == 4:
+              return True
+         else:
+              return False
+          
+    def addFile(nFile,fxs,fpardir):
+         if searchDir(fxs,fpardir):
+            if fxs:
+                if isFile(nFile):
+                    if data(data(fxs)) == fpardir:
+                           if(n1(fxs)):
+                                addFile(nFile,n1(fxs),fpardir)
+                           else:
+                                return Cons(data(fxs),Cons(nFile),n2(fxs),n3(fxs),n4(fxs))
+                           if n2(fxs):
+                                addFile(nFile,n2(fxs),fpardir)
+                           else:
+                                return Cons(data(fxs),n1(fxs),Cons(nFile),n3(fxs),n4(fxs))
+                           if n3(fxs):
+                                addFile(nFile,n3(fxs,fpardir))
+                           else:
+                                return Cons(data(fxs),n1(fxs),n2(fxs),Cons(nFile),n4(fxs))
+                           if n4(fxs):
+                                addFile(nFile,n4(fxs,fpardir))
+                           else:
+                                return Cons(data(fxs),n1(fxs),n2(fxs),n3(fxs),Cons(nFile))
+                    elif addFile(nFile,n1(fxs),fpardir):
+                         return Cons(data(fxs),addFile(nFile,n1(fxs),fpardir))
+                    elif addFile(nFile,n2(fxs),fpardir):
+                         return Cons(data(fxs),addFile(nFile,n2(fxs),fpardir))
+                    elif addFile(nFile,n3(fxs),fpardir):
+                         return Cons(data(fxs),addFile(nFile,n3(fxs),fpardir))
+                    elif addFile(nFile,n4(fxs),fpardir):
+                         return Cons(nFile,n4(fxs),fpardir)
+                           
+
     if type == 1:
-         return Cons(data(xs),addFile(ndata,xs,pardir))
-              
-         
+         return addFile(File(ndata),xs,pardir)
+'''   
 
 
 
@@ -125,10 +165,123 @@ def printTree(xs, prefix="", is_last=True):
             printTree(n4(xs), prefix + ("    " if is_last else "│   "), True)
 
 
+def isDir(xs:tuple)->bool:
+        if len(data(xs)) == 2:
+             return True
+        else:
+             return False
+
+
+    
+def addDirectory(fxs,fparDir,nDirectory):
+        def addOnDir(fxsa,nDirectorya):
+            if not n1(fxsa):
+                return Cons(data(fxsa),Cons(Directory(nDirectorya)),n2(fxsa),n3(fxsa),n4(fxsa))
+            elif not n2(fxsa):
+                return Cons(data(fxsa),n1(fxsa),Cons(Directory(nDirectorya)),n3(fxsa),n4(fxsa))
+            elif not n3(fxsa):
+                return Cons(data(fxsa),n1(fxsa), n2(fxsa) , Cons(Directory(nDirectorya)),n4(fxsa))
+            elif not n4(fxsa):
+                return Cons(data(fxsa),n1(fxsa),n2(fxsa),n3(fxsa),Cons(Directory(nDirectorya)))
+            else:
+                 print("Directory Full")
+            
+        if fxs and searchDir(fxs,fparDir):
+            if data(data(fxs)) == fparDir and isDir(fxs):
+                return addOnDir(fxs,nDirectory)
+            elif searchDir(n1(fxs),fparDir):
+                return Cons(data(fxs),addDirectory(n1(fxs),fparDir,nDirectory),n2(fxs),n3(fxs),n4(fxs))
+            elif searchDir(n2(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),addDirectory(n2(fxs),fparDir,nDirectory),n3(fxs),n4(fxs))
+            elif searchDir(n3(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),n2(fxs),addDirectory(n3(fxs),fparDir, nDirectory),n4(fxs))
+            elif searchDir(n4(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),n2(fxs),n3(fxs),addDirectory(n4(fxs),fparDir,nDirectory))
+
+        
+def addFile(fxs,fparDir,nFile):
+        def addOnDir(fxsa,nFilea):
+            if not n1(fxsa):
+                return Cons(data(fxsa),Cons(File(nFilea)),n2(fxsa),n3(fxsa),n4(fxsa))
+            elif not n2(fxsa):
+                return Cons(data(fxsa),n1(fxsa),Cons(File(nFilea)),n3(fxsa),n4(fxsa))
+            elif not n3(fxsa):
+                return Cons(data(fxsa),n1(fxsa), n2(fxsa) , Cons(File(nFilea)),n4(fxsa))
+            elif not n4(fxsa):
+                return Cons(data(fxsa),n1(fxsa),n2(fxsa),n3(fxsa),Cons(File(nFilea)))
+            
+        if fxs and searchDir(fxs,fparDir):
+            if data(data(fxs)) == fparDir and isDir(fxs):
+                return addOnDir(fxs,nFile)
+            elif searchDir(n1(fxs),fparDir):
+                return Cons(data(fxs),addFile(n1(fxs),fparDir,nFile),n2(fxs),n3(fxs),n4(fxs))
+            elif searchDir(n2(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),addFile(n2(fxs),fparDir,nFile),n3(fxs),n4(fxs))
+            elif searchDir(n3(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),n2(fxs),addFile(n3(fxs),fparDir, nFile),n4(fxs))
+            elif searchDir(n4(fxs),fparDir):
+                return Cons(data(fxs),n1(fxs),n2(fxs),n3(fxs),addFile(n4(fxs),fparDir,nFile))
+
+
+
+'''
+def add_folder_on_folder(sub_tree, new_node):
+  if(left(sub_tree) is Nil):
+    return Node(update_elements(sub_tree),Node(Folder(new_node)),midleft(sub_tree),midright(sub_tree),right(sub_tree))
+  elif(midleft(sub_tree) is Nil):
+    return Node(update_elements(sub_tree),left(sub_tree),Node(Folder(new_node)),midright(sub_tree),right(sub_tree))
+  elif(midright(sub_tree) is Nil):
+    return Node(update_elements(sub_tree), left(sub_tree),midleft(sub_tree), Node(Folder(new_node)),right(sub_tree))
+  elif(right(sub_tree) is Nil):
+    return Node(update_elements(sub_tree), left(sub_tree),midleft(sub_tree),midright(sub_tree), Node(Folder(new_node)))
+
+def add_file_on_folder(sub_tree: tuple, new_file_name: str, weight: int):
+  data_file= data_name_file(new_file_name)
+  if(left(sub_tree) is Nil):
+    return Node(object(sub_tree), Node(File(data_file[0],data_file[1],weight)),midleft(sub_tree),midright(sub_tree),right(sub_tree))
+  elif(midleft(sub_tree) is Nil):
+    return Node(object(sub_tree),left(sub_tree),Node(File(data_file[0],data_file[1],weight)),midright(sub_tree),right(sub_tree))
+  elif( midright(sub_tree) is Nil):
+    return Node(object(sub_tree), left(sub_tree),midleft(sub_tree), Node(File(data_file[0],data_file[1],weight)),right(sub_tree))
+  elif(right(sub_tree) is Nil):
+    return Node(object(sub_tree), left(sub_tree),midleft(sub_tree),midright(sub_tree), Node(File(data_file[0],data_file[1],weight)))
+
+def add_folder(sub_tree, folder_name, new_name, choice):
+  if(sub_tree and search_node(sub_tree,folder_name)):
+      if(name_object(sub_tree)==folder_name and is_folder(sub_tree)):
+        return add_folder_on_folder(sub_tree,new_name)
+      elif(search_node(left(sub_tree), folder_name)):
+        return Node(object(sub_tree), add_folder(left(sub_tree), folder_name, new_name,choice),midleft(sub_tree),midright(sub_tree),right(sub_tree))
+      elif(search_node(midleft(sub_tree),folder_name)):
+        return Node(object(sub_tree), left(sub_tree), add_folder(midleft(sub_tree), folder_name,new_name,choice),midright(sub_tree),right(sub_tree))
+      elif(search_node(midright(sub_tree),folder_name)):
+        return Node(object(sub_tree), left(sub_tree), midleft(sub_tree),add_folder(midright(sub_tree), folder_name, new_name,choice),right(sub_tree))
+      elif((search_node(right(sub_tree), folder_name))):
+        return Node(object(sub_tree), left(sub_tree), midleft(sub_tree),midright(sub_tree),add_folder(right(sub_tree),folder_name,new_name,choice))
+  else:
+      return
+    
+def add_file(sub_tree, folder_name, new_file_name, weight):
+  if(sub_tree and search_node(sub_tree,folder_name)):
+      if(name_object(sub_tree)==folder_name and is_folder(sub_tree)):
+        return add_file_on_folder(sub_tree, new_file_name,weight)
+      elif(search_node(left(sub_tree), folder_name)):
+        return Node(object(sub_tree), add_file(left(sub_tree), folder_name, new_file_name,weight),midleft(sub_tree),midright(sub_tree),right(sub_tree))
+      elif(search_node(midleft(sub_tree),folder_name)):
+        return Node(object(sub_tree), left(sub_tree), add_file(midleft(sub_tree), folder_name, new_file_name,weight),midright(sub_tree),right(sub_tree))
+      elif(search_node(midright(sub_tree),folder_name)):
+        return Node(object(sub_tree), left(sub_tree), midleft(sub_tree),add_file(midright(sub_tree), folder_name, new_file_name,weight),right(sub_tree))
+      elif((search_node(right(sub_tree), folder_name))):
+        return Node(object(sub_tree), left(sub_tree), midleft(sub_tree),midright(sub_tree),add_file(right(sub_tree),folder_name,new_file_name, weight))
+  else:
+      return
+
+'''
 
 
 
 if __name__ == '__main__':
     X = Cons(Directory("Root"),Cons(Directory("Dir1"),Cons(File("file1.txt 42")),Cons(File("fil.cpp 1"))),Cons(Directory("Dir2")),Cons(File("aa.jpeg 52")))
-    Y = addB("aa.txt 12",X,1,"Dir1")
-    printTree(Y)      
+    Y = addDirectory(X,"Root*","aaaa")
+    Z = addFile(Y,"aaaa*","asasa.aa 12")
+    printTree(Z)      
--- menu.py
+++ menu.py
@@ -3,25 +3,44 @@ def Menu():
     while True:
         print("Select an option\n1)Show all files/directories\n2)Add new file\n3)Add new directory\n4)Modify existing file\n5)Modify existing directory\n0)Exit")
         menuOption = input()
-        if menuOption is 0:
+        if menuOption == 0:
             print("Closing system...\n")
             break
-        elif menuOption is 1:
+        elif menuOption == 1:
             pass
-        elif menuOption is 2:
+        elif menuOption == 2:
             pass
-        elif menuOption is 3:
+        elif menuOption == 3:
             pass
-        elif menuOption is 4:
+        elif menuOption == 4:
             while True:
                 print("**** Select an option ****\n1)Delete a file\n2)Change file attributes\n0)Cancel")
                 fileMenuOption = input()
-                if fileMenuOption is 0:
+                if fileMenuOption == 0:
                     print("Cancelling...")
                     break
-                elif fileMenuOption is 1:
+                elif fileMenuOption == 1:
                     pass
-                elif fileMenuOption is 2:
+                elif fileMenuOption == 2:
                     pass
                 else:
                     print("//////ERROR////// \n invalid option, please select a valid one")
+        elif menuOption == 5:
+            while True:
+                print("**** Select an option ****\n1)Delete a directory\n2)Change directory name\n0)Cancel")
+                dirMenuOption = input()
+                if dirMenuOption == 0:
+                    print("Cancelling...")
+                    break
+                elif dirMenuOption == 1:
+                    pass
+                elif dirMenuOption == 2:
+                    pass
+                else:
+                    print("//////ERROR////// \n invalid option, please select a valid one")
+        else:
+            print("//////ERROR////// \n invalid option, please select a valid one")
+
+
+if __name__ == '__main__':
+    Menu()
